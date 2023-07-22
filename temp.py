c82f74716d10aa542bc2b5bb59e029be1abe35cf
akshaykrishna650 <115461410+akshaykrishna650@users.noreply.github.com>
2023-07-22 16:15:11 +0530

    Initial commit

---
+++ temp.py
@@ -0,0 +1,6 @@
+from sketchpy import canvas
+from sketchpy import library
+obj=canvas.sketch_from_image(r"C:\Users\acer\Desktop\project 01\car.png")
+obj.draw(threshold=110)
+# obj = library.rdj()
+# obj.draw()
